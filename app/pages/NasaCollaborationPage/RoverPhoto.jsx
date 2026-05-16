ee0946429e175e7a053ef595ea91743222dd54b8
niña <ninamarieondevilla@gmail.com>
2026-05-16 04:29:01 +0200

    Remove duplicate pages folder and fix Next build

--- app/pages/NasaCollaborationPage/RoverPhoto.jsx
+++
@@ -1,9 +0,0 @@
-export const RoverPhoto = ({ src, date, roverName }) => {
-  return (
-    <div>
-      <img src={src} alt={roverName} width="200" />
-      <p>{roverName}</p>
-      <p>{date}</p>
-    </div>
-  );
-};
